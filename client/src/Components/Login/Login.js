675c67656afbf785dd6bdbdb2378466c7371f025
davidc4489 <davidc4489@gmail.com>
2024-07-31 17:09:59 +0300

    login documentation

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -11,14 +11,20 @@ const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [permission, setPermission] = useState("");
+
+    // List of different existing authorizations
     const permissions = ['עובד', 'מנהל']
     
     const [showUpdatePersonalInformationsDialog, setShowUpdatePersonalInformationsDialog] = useState(false)
 
+    // Retrieves data and authentication functions from user context
     const { login, logout, user, access, accessAlert, setAccessAlert } = useAuth();
+
+    //  Get active user data from context
     const { name } = useUserUpdate();
 
     const submitHandler = (event) => {
+        // Check the user data with the context function
         login(email, password, permission)
         setEmail("");
         setPassword("");
@@ -42,6 +48,7 @@ const Login = () => {
         <>
         {showUpdatePersonalInformationsDialog ? <UpdatePersonalInformations OpenClose={openUpdatePersonalInformationsDialog}/> : null}  
         <div className='loginbox shadow-lg p-3 mb-5 bg-body rounded'>
+            {/* Case where no user identified */}
             {access !== true?
             <>
                 {(accessAlert) &&
@@ -58,6 +65,7 @@ const Login = () => {
                 </form>
             </>
         :        
+            // Case where user identified
             <>
                     {(name !== null) &&
                         <div className="alert alert-success" role="alert">
