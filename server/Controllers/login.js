21ed47c35a472d24e64022905d5d14c3f6010372
davidc4489 <davidc4489@gmail.com>
2024-08-05 23:45:03 +0300

    documentation

--- server/Controllers/login.js
+++ server/Controllers/login.js
@@ -9,15 +9,15 @@ export const checkLogin = async (req, res) => {
   let usersData = [] 
   await getAll("users").then(data => usersData = data)
 
-  // Vérifiez les informations d'identification de l'utilisateur
+  // Vérification des informations d'identification de l'utilisateur
   let authenticatedUser = usersData.find(user => user.מייל === mail && user.סיסמה === password && user.הרשאה === permission)
 
   if (authenticatedUser) {
-    // Comparez les mots de passe en utilisant bcrypt
+    // Comparation des mots de passe en utilisant bcrypt
     // const match = await bcrypt.compare(password, authenticatedUser.סיסמה);
 
     // if (match) {
-        // Créez le token JWT
+        // Création du token JWT
         const token = jwt.sign(
           { id: authenticatedUser.id, הרשאה: authenticatedUser.הרשאה }, // Payload
           secretKey,                        // Clé secrète
@@ -25,8 +25,8 @@ export const checkLogin = async (req, res) => {
         );
   
         res.json({
-          token, // Envoyez le token au client
-          authenticatedUser   // Envoyez aussi les informations utilisateur si nécessaire
+          token, // Envoie du token au client
+          authenticatedUser   // Envoie des informations utilisateur
         });
       } else {
         res.status(401).json({ message: 'Identifiants invalides' });
